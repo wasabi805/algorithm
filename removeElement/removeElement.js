0c91f7f50297329209021100fbb9a7f05b92de78
‘Tim <timothy.j.ocampo@gmail.com>
2019-08-06 13:35:49 -0700

    Leetcode alogoritm solutions :  added a second solution for removeElement problem . This solution involves using two pointers. Read the comments I left in the problem. Note: The first comment should read 'if current pointer does not match an instance OF val... , not IF val.'

--- removeElement/removeElement.js
+++ removeElement/removeElement.js
@@ -25,20 +25,45 @@
 let ex1 = [0,1,2,2,3,0,4,2], val1 = 2;
 
 
+// var removeElement = function(nums, val) {
+//
+//     //will have to loop backwards since splice() will
+//     // skip an index and also change the length of the array when looping forwards.
+//
+//     for(var i = nums.length-1 ; i>=0; i--){
+//         if(nums[i] === val){
+//             // console.log(i, 'index');
+//             nums.splice(i,1)
+//         }
+//
+//     }
+//     // console.log(nums.length)
+//     return nums.length
+// };
+//
+// removeElement(ex1, val1 );
+
+
+//FASTER SOLUTION: Two Pointers
+
 var removeElement = function(nums, val) {
+    var slowPointer = 0;
 
-    //will have to loop backwards since splice() will
-    // skip an index and also change the length of the array when looping forwards.
+    for(var currentPointer = 0; currentPointer<nums.length; currentPointer++ ){
 
-    for(var i = nums.length-1 ; i>=0; i--){
-        if(nums[i] === val){
-            // console.log(i, 'index');
-            nums.splice(i,1)
+        if(nums[currentPointer] !== val){  //if current pointer does not match an instance if val inside the nums array...
+
+            nums[slowPointer] = nums[currentPointer]; // increment both indexes at the same time
+            slowPointer++;// move to the next element in nums array
         }
 
+        //if done correctly, the value of var slowPointer should reflect the number of times that an element in the nums
+        //array was NOT EQUAL to the target.
     }
-    // console.log(nums.length)
-    return nums.length
+
+    return slowPointer
 };
 
 removeElement(ex1, val1 );
+
+
